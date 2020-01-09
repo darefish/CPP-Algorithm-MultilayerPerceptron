005d964c91aed9be7f8e53bab95d698f90ace347
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-08 18:03:58 -0800

    Update ActivationFunction.hpp

--- src/Function/ActivationFunction.hpp
+++ src/Function/ActivationFunction.hpp
@@ -1,1 +1,22 @@
+#ifndef ActivationFunction_hpp
+#define ActivationFunction_hpp
 
+#include <cmath>
+
+class ActivationFunction {
+public:
+    
+    static long double sigmoid(long double value);
+    
+    static long double derivativeSigmoid(long double value);
+    
+    static long double hyperbolicTangent(long double value);
+    
+    static long double derivativeHyperbolicTangent(long double value);
+    
+    static long double RELU(long double value);
+    
+    static long double derivativeRELU(long double value);
+};
+
+#endif
