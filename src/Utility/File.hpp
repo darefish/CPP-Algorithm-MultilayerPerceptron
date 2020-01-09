ab2d9f7d0f96c5d85f95a89dd7913e28cc4c5b94
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-08 17:29:30 -0800

    Update File.hpp

--- src/Utility/File.hpp
+++ src/Utility/File.hpp
@@ -1,1 +1,15 @@
+#ifndef File_hpp
+#define File_hpp
 
+#include <vector>
+
+#include <fstream>
+
+class File {
+public:
+    static std::vector<std::vector<long double>> readAsMatrix(const char* directory,unsigned long long int length);
+    
+    static std::vector<long double> readAsVector(const char* directory);
+};
+
+#endif
