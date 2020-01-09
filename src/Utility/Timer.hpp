5565b6f9d5050ae32632f79c12142feacc53d7d9
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-08 17:30:03 -0800

    Update Timer.hpp

--- src/Utility/Timer.hpp
+++ src/Utility/Timer.hpp
@@ -1,1 +1,25 @@
+#ifndef Timer_hpp
+#define Timer_hpp
 
+#include <chrono>
+
+class Timer {
+public:
+    const char* process;
+    
+    std::chrono::time_point<std::chrono::steady_clock> begin,end;
+    
+    std::chrono::duration<long double> duration;
+    
+    Timer();
+    
+    Timer(const char* process);
+    
+    void start();
+    
+    void stop();
+    
+    ~Timer();
+};
+
+#endif
