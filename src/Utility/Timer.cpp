1f9ee21c22465a2f4adddb8fd1c198a2420a1eb7
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-08 17:29:47 -0800

    Update Timer.cpp

--- src/Utility/Timer.cpp
+++ src/Utility/Timer.cpp
@@ -1,1 +1,23 @@
+#include "Timer.hpp"
 
+Timer::Timer() {
+    process="Process";
+}
+
+Timer::Timer(const char* process) {
+    this->process=process;
+}
+
+void Timer::start() {
+    begin=std::chrono::steady_clock::now();
+}
+
+void Timer::stop() {
+    end=std::chrono::steady_clock::now();
+    
+    duration=end-begin;
+}
+
+Timer::~Timer() {
+    
+}
