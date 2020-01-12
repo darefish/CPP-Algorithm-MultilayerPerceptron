6db18f0af4b7107e7cd3589be08d5d696126685e
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-11 16:59:41 -0800

    Update Neuron.hpp

--- src/Essence/Neuron.hpp
+++ src/Essence/Neuron.hpp
@@ -1,1 +1,25 @@
+#ifndef Neuron_hpp
+#define Neuron_hpp
 
+#include <vector>
+
+class Neuron {
+public:
+    long double value=0.0;
+    
+    long double bias=0.0;
+    
+    std::vector<Neuron*> connections;
+    
+    std::vector<long double> weights;
+    
+    Neuron();
+    
+    void connect(Neuron* neuron);
+    
+    void activate();
+    
+    ~Neuron();
+};
+
+#endif
