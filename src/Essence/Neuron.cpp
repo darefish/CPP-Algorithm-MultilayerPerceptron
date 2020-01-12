e361261656b4c52b55a0d893e424a9682e2c0b90
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-11 16:59:24 -0800

    Update Neuron.cpp

--- src/Essence/Neuron.cpp
+++ src/Essence/Neuron.cpp
@@ -1,1 +1,20 @@
+#include "Neuron.hpp"
 
+Neuron::Neuron() {
+    
+}
+
+void Neuron::connect(Neuron* neuron) {
+    connections.push_back(neuron);
+    weights.push_back((long double)rand()/(long double)RAND_MAX);
+}
+
+void Neuron::activate() {
+    for(auto i=0;i<connections.size();++i) {
+        connections[i]->value+=this->value*weights[i];
+    }
+}
+
+Neuron::~Neuron() {
+    
+}
