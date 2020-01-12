89b922fd0b47ed03f37521e4e3df332bb7aa7b57
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-11 16:58:51 -0800

    Update Matrix.cpp

--- src/Operation/Matrix.cpp
+++ src/Operation/Matrix.cpp
@@ -1,1 +1,25 @@
+#include "Matrix.hpp"
 
+std::vector<std::vector<long double>> Matrix::isolationOfMatrix(std::vector<std::vector<std::vector<long double>>>& vector,unsigned long long int layer) {
+    std::vector<std::vector<long double>> output;
+    
+    for(unsigned long long int i=0;i<vector.size();++i) {
+        output.push_back(vector[i][layer]);
+    }
+    
+    return output;
+}
+
+std::vector<std::vector<long double>> Matrix::averageOfMatrix(std::vector<std::vector<std::vector<long double>>>& vector) {
+    
+    std::vector<std::vector<long double>> output;
+    
+    for(unsigned long long int i=0;i<vector[0].size();++i) {
+        
+        std::vector<std::vector<long double>> local=isolationOfMatrix(vector,i);
+        
+        output.push_back(Vector::averageOfVector(local));
+    }
+    
+    return output;
+}
