001f8491b735410c8476ecd9dc14a9184ecc3244
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-11 16:59:04 -0800

    Update Matrix.hpp

--- src/Operation/Matrix.hpp
+++ src/Operation/Matrix.hpp
@@ -1,1 +1,15 @@
+#ifndef Matrix_hpp
+#define Matrix_hpp
 
+#include <vector>
+
+#include "Vector.hpp"
+
+class Matrix {
+public:
+    static std::vector<std::vector<long double>> isolationOfMatrix(std::vector<std::vector<std::vector<long double>>>& vector,unsigned long long int layer);
+    
+    static std::vector<std::vector<long double>> averageOfMatrix(std::vector<std::vector<std::vector<long double>>>& vector);
+};
+
+#endif
