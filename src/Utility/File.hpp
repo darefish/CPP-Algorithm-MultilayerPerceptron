218425b1dfd9f846310156025717a3bcc6cecf81
Dominic Jiang <49256436+dominicjiang@users.noreply.github.com>
2020-01-12 23:47:03 -0800

    Update File.hpp

--- src/Utility/File.hpp
+++ src/Utility/File.hpp
@@ -10,6 +10,8 @@ public:
     static std::vector<std::vector<long double>> readAsMatrix(const char* directory,unsigned long long int interval);
     
     static std::vector<long double> readAsVector(const char* directory);
+    
+    static void writeAsVector(const char* directory,std::vector<long double>& vector);
 };
 
 #endif
